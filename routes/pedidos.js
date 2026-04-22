7b4fd909bf5008670e940286e01e769788f18d56
Eder Daniel Domingues <eder.infonet@gmail.com>
2026-04-22 07:09:28 -0400

    Implementação das rotas

---
+++ routes/pedidos.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const router = express.Router()
+
+// Criação das rotas
+router.get("/", (req, res, next) => {
+  res.status(200).send({
+    mensagem: "Só Jesus Salva",
+  })
+})
+
+// Insere um pedido no BD
+router.post("/", (req, res, next) => {
+  res.status(201).send({
+    mensagem: "O pedido foi criado",
+  })
+})
+
+// Busca um pedido pelo id
+router.get("/:id_pedido", (req, res, next) => {
+  const id = req.params.id_pedido
+  res.status(200).send({
+    mensagem: "Retorna os detalhes de um pedido",
+    id_pedido: id,
+  })
+})
+
+// Exclui um pedido
+router.delete("/", (req, res, next) => {
+  res.status(200).send({
+    mensagem: "Pedido excluído",
+  })
+})
+
+module.exports = router
--- routes/produtos.js
+++ routes/produtos.js
@@ -1,24 +1,27 @@
 const express = require("express")
 const router = express.Router()
 
+// Criação das rotas
 router.get("/", (req, res, next) => {
   res.status(200).send({
-    mensagem: "Só Jesus Salva",
+    mensagem: "Retorna todos os produtos",
   })
 })
 
+// Insere um produto no BD
 router.post("/", (req, res, next) => {
   res.status(201).send({
-    mensagem: "Com Deus para vencer",
+    mensagem: "Produto inserido com sucesso",
   })
 })
 
+// Busca um produto pelo id
 router.get("/:id_produto", (req, res, next) => {
   const id = req.params.id_produto
 
   if (id === "especial") {
     res.status(200).send({
-      mensagem: "Jesus Salva e Guia meus passos",
+      mensagem: "Retorna um pedido pelo id",
       id: id,
     })
   } else {
@@ -26,6 +29,20 @@ router.get("/:id_produto", (req, res, next) => {
       mensagem: "Sem Jesus o caminho está errado",
     })
   }
+})
+
+// Alteração de produtos
+router.patch("/", (req, res, next) => {
+  res.status(201).send({
+    mensagem: "Produto alterado",
+  })
+})
+
+// Exclui um produto
+router.delete("/", (req, res, next) => {
+  res.status(200).send({
+    mensagem: "Produto excluído",
+  })
 })
 
 module.exports = router
